3d15db8d9701c8a10c8c5178473300f3bdefa579
juampi <jpmiya82@gmail.com>
2026-03-06 00:14:56 -0300

    fixes

--- core/config.py
+++ core/config.py
@@ -2,6 +2,4 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parents[1]
 
-FRONTEND_DIR = BASE_DIR / "frontend"
-DASHBOARD_DIR = FRONTEND_DIR / "dashboard"
 IMAGES_DIR = BASE_DIR / "images"
--- main.py
+++ main.py
@@ -1,27 +1,16 @@
 from fastapi import FastAPI
 from core.lifespan import lifespan
 from core.middleware import setup_middleware
-from routers import frontend, metrics, chat, image
-from core.config import FRONTEND_DIR, DASHBOARD_DIR
-from fastapi.staticfiles import StaticFiles
-from orders import router as orders_router
+from routers import metrics, chat, image
 
 
 def create_app() -> FastAPI:
-    # Configuraciones
     app = FastAPI(lifespan=lifespan)
     setup_middleware(app)
 
-    # Montados estaticos
-    app.mount("/chat/static", StaticFiles(directory=str(FRONTEND_DIR)), name="chat-static")
-    app.mount("/dashboard/static", StaticFiles(directory=str(DASHBOARD_DIR)), name="dashboard-static")
-
-    # Rutas
-    app.include_router(frontend.router)
     app.include_router(metrics.router)
     app.include_router(chat.router)
     app.include_router(image.router)
-    app.include_router(orders_router)
 
     return app
 
